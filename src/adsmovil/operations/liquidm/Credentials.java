2a5712a3b68da55499dda625c2b964ab68dee51c
Jefferson Johannes Roth Filho <jjrothfilho@gmail.com>
2023-03-04 05:16:31 -0500

    Update Credentials.java

--- src/adsmovil/operations/liquidm/Credentials.java
+++ src/adsmovil/operations/liquidm/Credentials.java
@@ -2,7 +2,7 @@ package adsmovil.operations.liquidm;
 
 public class Credentials {
 	
-	static String LIQUIDM_EMAIL = "jefferson@adsmovil.com";
-	static String LIQUIDM_PASSWORD = "liquidMDunkelh3it";
+	static String LIQUIDM_EMAIL;
+	static String LIQUIDM_PASSWORD";
 
 }
